547dc68999ac0c2a0288e27ba7ecf0b4a3faf9f7
Anna Agafonova <aa.agafonova@gmail.com>
2021-03-09 12:11:02 +0300

    gallery: slick + magnific-popup

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,2 +1,4 @@
-@@include('swiper-bundle.js');
-@@include('swiper.js');
+@@include('jquery.min.js');
+@@include('slick.min.js');
+@@include('magnific-popup.js');
+@@include('slider.js');
---
+++ #src/js/slider.js
@@ -0,0 +1,52 @@
+jQuery(function($){
+    sliderCall.photoGallery();
+    lightboxTrigger();
+});
+       
+var sliderCall = {
+    photoGallery: function() {
+        var slider = $(".gallery-slider");
+        slider.slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerMode: true,
+                        centerPadding: '15px'
+                    }
+                }
+            ]
+        });
+    }
+}
+
+function lightboxTrigger() {
+    $('.popup-gallery').magnificPopup({
+		delegate: 'li a',
+		type: 'image',
+        fixedContentPos: true,
+        closeOnBgClick: true,
+        alignTop: false,
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+}

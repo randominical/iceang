2c25d6cfa3e1ed8f1384ce8da06cad5b21fdd789
Anna Agafonova <aa.agafonova@gmail.com>
2021-04-04 12:34:45 +0300

    slider разная ширина слайдов, меню home, лого больше
    проблема с обрезание тени в слайдере,
    проблема с искажением высоты открытого лайтбокса при уменьшении высоты окна

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,9 +30,10 @@ var sliderCall = {
             centerMode: true,
             infinite: true,
             //autoplay: true,
-            slidesToShow: 4,
+            slidesToShow: 1,
             slidesToScroll: 1,
             draggable: true,
+            variableWidth: true,
             responsive: [
                 {
                     breakpoint: 768,
@@ -58,19 +59,41 @@ function lightboxTrigger() {
 		delegate: 'li a',
 		type: 'image',
         image: {
-            titleSrc: function(item) {
-                return item.el.find('img').attr('title');
-             }
+            verticalFit: true,
         },
         fixedContentPos: true,
         closeOnBgClick: true,
         alignTop: false,
 		tLoading: 'Loading image #%curr%...',
 		mainClass: 'mfp-img-mobile',
+        overflowY: true,
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
 			preload: [1,1]
 		}
 	});
-};
+}
+
+/*
+image: {
+    markup: '<div class="mfp-figure">'+
+              '<div class="mfp-close"></div>'+
+              '<div class="mfp-img"></div>'+
+              '<div class="mfp-bottom-bar">'+
+                '<div class="mfp-title"></div>'+
+                '<div class="mfp-counter"></div>'+
+              '</div>'+
+            '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+  
+    cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+  
+    titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
+    // Or the function that should return the title. For example:
+    // titleSrc: function(item) {
+    //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    // }
+  
+    verticalFit: true, // Fits image in area vertically
+  
+    tError: '<a href="%url%">The image</a> could not be loaded.' */;

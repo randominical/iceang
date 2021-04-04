9276bc03c4e55235f94cd2b0b6ee491ef716392a
Anna Agafonova <aa.agafonova@gmail.com>
2021-04-04 20:25:26 +0300

    slider оформление тени

--- #src/js/slider.js
+++ #src/js/slider.js
@@ -48,7 +48,6 @@ function lightboxTrigger() {
         alignTop: false,
 		tLoading: 'Loading image #%curr%...',
 		mainClass: 'mfp-img-mobile',
-        overflowY: true,
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
@@ -56,26 +55,3 @@ function lightboxTrigger() {
 		}
 	});
 }
-
-/*
-image: {
-    markup: '<div class="mfp-figure">'+
-              '<div class="mfp-close"></div>'+
-              '<div class="mfp-img"></div>'+
-              '<div class="mfp-bottom-bar">'+
-                '<div class="mfp-title"></div>'+
-                '<div class="mfp-counter"></div>'+
-              '</div>'+
-            '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
-  
-    cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
-  
-    titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
-    // Or the function that should return the title. For example:
-    // titleSrc: function(item) {
-    //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-    // }
-  
-    verticalFit: true, // Fits image in area vertically
-  
-    tError: '<a href="%url%">The image</a> could not be loaded.' */
